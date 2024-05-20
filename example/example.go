857fdd44f473ae7027ea18eb376e3a7c713f8f68
Dmytro Yaroshenko <73843436+o-murphy@users.noreply.github.com>
2024-05-20 15:13:08 +0300

    added example

---
+++ example/example.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+
+	pystruct "github.com/o-murphy/pystruct-go"
+)
+
+func main() {
+
+	// unpack
+	byteArray := []byte{97, 98, 99, 100, 101, 102, 103}
+	if intf, err := pystruct.Unpack(`<3sf`, byteArray); err == nil {
+		fmt.Println(intf...)
+	}
+
+	// pack
+
+}
---
+++ example/go.mod
@@ -0,0 +1,5 @@
+module example
+
+go 1.19
+
+require github.com/o-murphy/pystruct-go v0.0.0-20240520120747-2bbc587782ae
