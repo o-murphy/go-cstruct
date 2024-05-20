d09634f36807db9ece3fbf5c6fb5e760ebbe745d
Dmytro Yaroshenko <73843436+o-murphy@users.noreply.github.com>
2024-05-20 17:37:19 +0300

    example upd

--- example/example.go
+++ example/example.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
 
 	pystruct "github.com/o-murphy/pystruct-go"
@@ -10,13 +11,19 @@ func main() {
 
 	// unpack
 	byteArray := []byte{97, 98, 99, 100, 101, 102, 103}
-	if intf, err := pystruct.Unpack(`<3sf`, byteArray); err == nil {
+	intf, err := pystruct.Unpack(`<3sf`, byteArray)
+	if err == nil {
 		fmt.Println(intf...)
 	}
 
 	// pack
-	if byteArray2, err := pystruct.Pack(`<3sf`, intf); err == nil {
+	byteArray2, err := pystruct.Pack(`<3sf`, intf)
+	if err == nil {
 		fmt.Println(byteArray2)
+	}
+
+	if bytes.Equal(byteArray, byteArray2) {
+		fmt.Println("Equal")
 	}
 
 }
--- example/go.mod
+++ example/go.mod
@@ -1,3 +1,5 @@
 module example
 
 go 1.19
+
+require github.com/o-murphy/pystruct-go v0.0.0-20240520143156-f6a4b017a0c8 // indirect
